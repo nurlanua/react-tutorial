306a0a73666147e79b389e4b38e4d88d03040f8c
Azat Nurlan Uulu <73550146+nurlanua@users.noreply.github.com>
2025-05-25 07:42:24 +0000

    done till 51:02

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,20 +1,26 @@
-import { Fragment } from "react";
+import { useState } from "react";
 
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
-    < >
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
-          
-    </ >
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>Item Not Found</p>}
+      <ul className="list-group">
+        {items.map((item,index) => (
+          <li
+            className={ selectedIndex === index ? 'list-group-item active': 'list-group-item'}
+            key={item}
+            onClick={() => {setSelectedIndex(index); }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
-
 
 export default ListGroup;
